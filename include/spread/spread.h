ec749c3300f976431d45afd9629c772e9b3d0899
shiguangpeng <742508176@qq.com>
2024-03-06 09:25:15 +0800

    :sparkles: 完成除打开外的功能

--- include/spread/spread.h
+++ include/spread/spread.h
@@ -64,7 +64,7 @@ namespace spread {
     /**
      * @brief 分析环境参数结构体，封装了对环境参数的get与set
      */
-    struct AnalyseEnvironmentST* pEnvi;
+    struct AnalyseEnvironmentST *pEnvi;
 
     /**
      * @brief 高程栅格路径
@@ -80,7 +80,7 @@ namespace spread {
     std::string errorInfo;
 
     // 栅格数据对象
-    // IFileFloatArray*pElevData;
+    IFileFloatArray *pElevData;
 
     /**
      * @brief 栅格列数
@@ -128,19 +128,22 @@ namespace spread {
 
   class CFieldStrengthAnalyse : public CSpreadAnalyse {
   protected:
-    // IStations*pStations;
+    IStations *pStations;
     float_t hm;
     std::string outputPath;
-    // IFileFloatArray*pData;
+    IFileFloatArray *pData;
     float_t offsetDB;
     bool needComputeAll;
     std::vector<double_t> crsv;
     Station stationInfo;
-    double SubExtent[4];
+    /// @brief 分析范围，默认是整个dem，每个子类有自己的get/putExtent方法，为该数组设置值
+    double_t subExtent[4];
 
   public:
     CFieldStrengthAnalyse();
     virtual ~CFieldStrengthAnalyse();
+    // 场强分析算法的调用入口，每个场强分析类都有自己的场强分析调用入口，重定义。
+    bool FieldStrengthAnalyse(std::string savePath, RasterCreateFileType type);
 
   protected:
     /* 子类必须实现的方法 */
@@ -149,7 +152,7 @@ namespace spread {
     /// @param rsv 动态数组，用于保存每个子类保存的数据，每次使用前需要留意数组中已经存在的数据
     /// @param point 待分析的点坐标
     /// @return 返回点对点之间场强分析的结果
-    virtual float GetRadiuValue(Station& stationInfo, std::vector<double>& rsv, OGRPoint& point)
+    virtual float_t GetRadiuValue(Station &stationInfo, std::vector<double_t> &rsv, OGRPoint &point)
         = 0;
 
     /// @brief
@@ -157,13 +160,14 @@ namespace spread {
     /// @param rsv
     /// @param point 待分析的点坐标
     /// @return
-    virtual float GetRadiuValueRev(Station& stationInfo, std::vector<double>& rsv, OGRPoint& point)
+    virtual float_t GetRadiuValueRev(Station &stationInfo, std::vector<double_t> &rsv,
+                                     OGRPoint &point)
         = 0;
 
     /// @brief
     /// @param stationInfo
     /// @param rsv 是一个数组，该数组里面装了台站的初始的发射功率以及其他相关信息
-    virtual void PrepareReservedValues(Station& stationInfo, std::vector<double>& rsv) = 0;
+    virtual void PrepareReservedValues(Station &stationInfo, std::vector<double_t> &rsv) = 0;
 
     /// @brief 分析所需要的其他栅格数据
     /// @return 返回准备的状态
@@ -172,6 +176,13 @@ namespace spread {
     /// @brief 获取模型的名称
     /// @return 返回模型的名称
     virtual std::string GetModelName() = 0;
+
+  protected:
+    void ComputeOneStation(Station &para, std::string ProInfo);
+    bool ComputeOneStationByExtent(Station &para, std::vector<double_t> &rsv);
+    int *GetSubRowCol(double xmin, double ymin, double xmax, double ymax);
+    // bool CFieldStrengthAnalyse::SaveOutput(IFileFloatArray *pData, RasterCreateFileType type,
+    //                                        RasterCreateDataType dtype);
   };
 
   /**
@@ -181,9 +192,14 @@ namespace spread {
    * @attention 该类的子类模型均有绕射衰减的计算，但是绕射衰减的计算是可选的，默认值为0
    */
   class CCombineAnalyse : public CFieldStrengthAnalyse {
+  public:
+    bool FieldStrengthAnalyse(std::string savePath, RasterCreateFileType type);
+
   protected:
-    float GetRadiuValue(Station& para, std::vector<double>& rsv, double X, double Y, float Z);
-    float GetRadiuValueRev(Station& para, std::vector<double>& rsv, double X, double Y, float Z);
+    float_t GetRadiuValue(Station &stationInfo, std::vector<double_t> &rsv, OGRPoint *point);
+
+    float_t GetRadiuValueRev(Station &para, std::vector<double_t> &rsv, double_t X, double_t Y,
+                             float_t Z);
 
     /// @brief
     /// 子类分析算法需要的其他数据，具体数据由该子类管理（如算法需要的特定图层，点等对象），但是给父类返回准备的结果
@@ -191,20 +207,26 @@ namespace spread {
     bool PrepareOtherData();
 
   protected:
-    // std::vector<IGDALRasterReaderByFileArray*>otherReaders;
-    // std::vector<IFileFloatArray*>otherDatas;
-    // std::vector<IDBLossElement*,IDBLossElement*>els;
+    std::vector<IGDALRasterReaderByFileArray *> otherReaders;
+    std::vector<IFileFloatArray *> otherDatas;
+    std::vector<IDBLossElement *, IDBLossElement *> els;
     std::vector<std::string> otherNames;
     std::vector<std::string> otherPaths;
-
-  protected:
-    float_t GetRadiuValue(Station& stationInfo, std::vector<double_t>& rsv, OGRPoint* point);
   };
 
-  /// @brief 自由空间传播模型，暂只实现必要方法
+  /**
+   * @brief 自由空间传播模型，暂只实现必要方法
+   * **/
   class CFreeSpaceAnalyse : public CCombineAnalyse {
   protected:
-    float_t GetRadiuValue(Station& stationInfo, std::vector<double_t>& rsv, OGRPoint* point);
+    float_t GetRadiuValue(Station &stationInfo, std::vector<double_t> &rsv, OGRPoint *point);
+
+  public:
+    // 调用父类CFreeSpaceAnalyse的FieldStrengthAnalyse方法
+    void CFreeSpaceAnalyse::FieldStrengthAnalyse(std::string savePath, RasterCreateFileType type,
+                                                 bool *pVal) {
+      *pVal = CCombineAnalyse::FieldStrengthAnalyse(savePath, type);
+    }
   };
 
   /**
@@ -212,7 +234,7 @@ namespace spread {
    */
   struct AnalyseEnvironmentST {
   private:
-    OGRPoint* leftTop;
+    OGRPoint *leftTop;
     double_t cellSize;
     long cols;
     long rows;
@@ -221,7 +243,7 @@ namespace spread {
   public:
     /// @brief 获取栅格左上角坐标
     /// @return OGRPoint*
-    virtual OGRPoint* GetLeftTop() = 0;
+    virtual OGRPoint *GetLeftTop() = 0;
     /// @brief 设置栅格左上角坐标
     /// @param point OGR坐标对象
     virtual void SetLetTop(OGRPoint point) = 0;
@@ -235,14 +257,14 @@ namespace spread {
 
     /// @brief 获取栅格的列数
     /// @return 栅格列总数
-    virtual long GetCols() = 0;
+    virtual void GetCols(long *cols) = 0;
     /// @brief 设置栅格的列数
     /// @param cols 栅格列数
     virtual void SetCols(long cols) = 0;
 
     /// @brief 获取栅格行数
     /// @return 栅格行数
-    virtual long GetRows() = 0;
+    virtual void GetRows(long *rows) = 0;
     /// @brief 设置栅格行数
     /// @param rows 栅格行数
     virtual void SetRows(long rows) = 0;
@@ -253,6 +275,172 @@ namespace spread {
     /// @brief 设置输出栅格的分辨率
     /// @param outPutCellSize 输出栅格分辨率
     virtual void SetOutputCellSize(double_t outPutCellSize) = 0;
+  };
+
+  /* ------------------ DBLossElement结构体，父结构体，保存绕射传播的各种参数 -------------------*/
+  struct IDBLossElement {
+  protected:
+    // 是否复杂模型
+    virtual void getComplicatedModel(_Bool *isComplicated) = 0;
+    // 设置CombineAnalyse
+    virtual void setCombineAnalyse(ICombineAnalyse *combineAnalyse) = 0;
+    virtual void prepareAnalyseEnvi(void) = 0;
+    virtual double_t GetDBLoss(Station station, OGRPoint point) = 0;
+    virtual double_t GetDBLossRev(Station station, OGRPoint point) = 0;
+  };
+
+  /** --------------------------- CDEMAnalyse对应的数据的封装 ----------------------------------*/
+  struct IDEMAnalyse {
+  public:
+    virtual void getAnalyseEnvi() = 0;
+  };
+  struct ICombineAnalyse : public IDEMAnalyse {};
+
+  /** --------------------------- 数据类型封装-------------------------------------------------*/
+  struct IDataArray {
+  public:
+    virtual std::string GetName(std::string pVal) = 0;
+    virtual void SetName(std::string newVal) = 0;
+
+    virtual long GetSize(long *pVal) = 0;
+    virtual void SetSize(long size, float_t initialValue, bool *pVal) = 0;
+
+    virtual void SetDefaultValue(float_t initialValue = 0) = 0;
+
+    virtual void GetValueAsFloat(long pos, float_t *pVal) = 0;
+    virtual void SetValueAsFloat(long pos, float_t newVal) = 0;
+
+    virtual void GetValuesAsByte(long fromPos, std::vector<double_t> **refData) = 0;
+    virtual void SetValuesAsByte(long fromPos, std::vector<double_t> *refData) = 0;
+
+    virtual void GetValuesAsShort(long fromPos, std::vector<double_t> **refData) = 0;
+    virtual void SetValuesAsShort(long fromPos, std::vector<double_t> *refData) = 0;
+
+    virtual void GetValuesAsInteger(long fromPos, std::vector<double_t> **refData) = 0;
+    virtual void SetValuesAsInteger(long fromPos, std::vector<double_t> *refData) = 0;
+
+    virtual void SetValuesAsFloat(long fromPos, std::vector<double_t> **refData) = 0;
+    virtual void SetValuesAsFloat(long fromPos, std::vector<double_t> *refData) = 0;
+  };
+
+  struct IFileDataArray : public IDataArray {
+  public:
+    virtual void GetType(FileDataArrayType *pVal) = 0;
+
+    virtual void GetDirectory(std::string pVal) = 0;
+    virtual void SetDirectory(std::string newVal) = 0;
+
+    virtual void GetBufferSize(FileArrayBufferSize *pVal) = 0;
+    virtual void SetBufferSize(FileArrayBufferSize newVal) = 0;
+  };
+
+  struct IStations {
+  public:
+    virtual void AddStation(Station station) = 0;
+
+    virtual void GetCount(long *pVal) = 0;
+
+    virtual void RemoveAt(long index) = 0;
+
+    virtual void RemoveAll(void) = 0;
+
+    virtual void GetItem(long index, Station *pVal) = 0;
+
+    virtual void PutItem(long index, Station newVal) = 0;
+
+    virtual void PickHeightFromDEM(IGDALRasterReaderByPixel *pReader, bool *pVal) = 0;
+  };
+
+  // 自定义矩形结构体类型
+  struct CRect {
+  public:
+    int left;
+    int top;
+    int right;
+    int bottom;
+
+  public:
+    CRect();
+    CRect(int left, int top, int right, int bottom);
+  };
+  /* ------------------------------------- 枚举类型定义 ----------------------------------------*/
+  typedef enum FileDataArrayTypeST {
+    fdatUnknown = 0,
+    fdatByte = 0x1,
+    fdatShort = 0x2,
+    fdatInteger = 0x3,
+    fdatFloat = 0x4
+  } FileDataArrayType;
+
+  typedef enum FileArrayBufferSizeST {
+    fabsBufferHalfMB = 0,
+    fabsBuffer1MB = 0x1,
+    fabsBuffer2MB = 0x2,
+    fabsBuffer4MB = 0x3,
+    fabsBuffer8MB = 0x4,
+    fabsBuffer16MB = 0x5,
+    fabsBuffer32MB = 0x6,
+    fabsBuffer64MB = 0x7
+  } FileArrayBufferSize;
+
+  // 栅格数据类型，可选tiff，erdas，envi，pci
+  typedef enum RasterCreateFileTypeST {
+    rcftTiff = 0,
+    rcftErdasImagine = 0x1,
+    rcftPCIPix = 0x2,
+    rcftEnviHdr = 0x3
+  } RasterCreateFileType;
+
+  typedef enum RasterCreateDataTypeST {
+    rcdtUnknown = 0,
+    rcdtByte = 0x1,
+    rcdtUInt16 = 0x2,
+    rcdtInt16 = 0x3,
+    rcdtUInt32 = 0x4,
+    rcdtInt32 = 0x5,
+    rcdtFloat32 = 0x6,
+    rcdtFloat64 = 0x7
+  } RasterCreateDataType;
+
+  /* ----------------------------- GDAL读取栅格数据的封装 ---------------------------------------*/
+  struct IGDALRasterReader {
+  public:
+    virtual void OpenRaster(std::string lpszPathName, bool *pVal) = 0;
+    virtual void SetRasterBand(GDALRasterBand band, bool *pVal) = 0;
+  };
+
+  struct IGDALRasterReaderByFileArray : public IGDALRasterReader {
+  public:
+    virtual void GetBlockData(long x1, long y1, long x2, long y2, long buffx, long buffy,
+                              IFileFloatArray **pVal)
+        = 0;
+
+    virtual void GetInterpolatedBlockData(long x1, long y1, long x2, long y2, long BuffX,
+                                          long BuffY, IFileFloatArray **pVal)
+        = 0;
+
+    // 将原来的ISpatialReference换成EPSG代码，不要原来封装的操作，太过繁琐，需要时直接调用GDAL的相关方法即可，不必再次封装。
+    virtual void SetRefTargetSpatialReference(int *newVal = 0) = 0;
+
+    virtual void GetBlockDataByCoord(OGRPoint *leftTop, double_t cellSize, long width, long height,
+                                     float_t noData, IFileFloatArray **pVal)
+        = 0;
+
+    virtual void GetInterpolatedBlockDataByCoord(OGRPoint *LeftTop, double_t CellSize, long Width,
+                                                 long Height, float_t NoData,
+                                                 IFileFloatArray **pVal)
+        = 0;
+  };
+
+  // 获取指定坐标处的dem值
+  struct IGDALRasterReaderByPixel : public IGDALRasterReader {
+  public:
+    virtual void GetPixelValue(long col, long row, float_t *pVal) = 0;
+  };
+
+  // why? 暂时根据原项目移植
+  struct IFileFloatArray : public IFileDataArray {
+  public:
   };
 
 }  // namespace spread
--- source/spread.cpp
+++ source/spread.cpp
@@ -8,9 +8,10 @@
 using namespace spread;
 using namespace spatidatamanager;
 
+/*** --------------------- 所有场强分析算法的基类，实现 ------------------------------- */
+
 CSpreadAnalyse::CSpreadAnalyse() {}
 CSpreadAnalyse::~CSpreadAnalyse() {}
-
 bool CSpreadAnalyse::InitEnvironment() {
   // 通过打开的栅格数据，初始化类相关成员。
   const char* path = elevationPath.c_str();
@@ -31,27 +32,25 @@ bool CSpreadAnalyse::InitEnvironment() {
   yMax = geoInfo[3];
   cellSize = geoInfo[1];
   // 获取当前tif图的所有波段数量
-   int bandNum = data->GetRasterCount();
+  int bandNum = data->GetRasterCount();
   // 波段从1开始，dem这种结果图层也只有一个图层
-    int *dataTypeFlag = nullptr;
-    double_t bandNoData = 0.0;
+  int* dataTypeFlag = nullptr;
+  double_t bandNoData = 0.0;
   for (int i = 1; i <= bandNum; i++) {
     GDALRasterBand* band = data->GetRasterBand(i);
     GDALDataType dataType = band->GetRasterDataType();
     // 根据GetNoDataValue()方法的说明，特定数据类型需要使用其他方法
-    if (dataType == GDALDataType::GDT_Int64)
-    {
-      bandNoData = band-> GetNoDataValueAsInt64(dataTypeFlag);
-    }else if(dataType == GDALDataType::GDT_UInt64){
+    if (dataType == GDALDataType::GDT_Int64) {
+      bandNoData = band->GetNoDataValueAsInt64(dataTypeFlag);
+    } else if (dataType == GDALDataType::GDT_UInt64) {
       bandNoData = band->GetNoDataValueAsUInt64(dataTypeFlag);
-    }else {
+    } else {
       bandNoData = band->GetNoDataValue(dataTypeFlag);
     }
     // todo: 就项目来说，像dem这种结果tiff图，土地利用类型等，一般只有一个波段。这里先break;
     break;
   }
-  if (dataTypeFlag)
-  {
+  if (dataTypeFlag) {
     noData = bandNoData;
   }
   // 结束前关闭数据集对象
@@ -59,4 +58,436 @@ bool CSpreadAnalyse::InitEnvironment() {
   GDALDestroy();
   errorInfo = "栅格数据打开成功。";
   return true;
+}
+
+/** ---------------------CFieldStrengthAnalyse 场强分析类的实现 ---------------------------------*/
+bool CFieldStrengthAnalyse::FieldStrengthAnalyse(std::string savePath, RasterCreateFileType type) {
+  errorInfo = "";
+  long Count;
+  pStations->GetCount(&Count);
+  if (Count == 0) {
+    errorInfo = "参与计算的台站数为0";
+    return false;
+  }
+  bool IsOk;
+  if (!CSpreadAnalyse::InitEnvironment()) {
+    return false;
+  }
+
+  if (!PrepareOtherData()) {
+    return false;
+  }
+
+  // COM组件的写法，要将接口的引用计数减1，当减为0时，会使用delete将该接口（struct）释放
+  // if (pData != NULL) {
+  //   pData->Release();
+  // }
+
+  long Cols, Rows;
+  pEnvi->GetCols(&Cols);
+  pEnvi->GetRows(&Rows);
+  pData->SetSize(Cols * Rows, noData, &IsOk);
+  if (!IsOk) {
+    errorInfo = "数组初始化失败";
+    return false;
+  }
+  for (int k = 0; k < Count; k++) {
+    Station para;
+    pStations->GetItem(k, &para);
+    ComputeOneStation(para);
+  }
+  // 保存结果，生成tiff
+  // if (!SaveOutput(pData, type, RasterCreateDataType::rcdtFloat32)) {
+  //   errorInfo = "保存数据失败, 可能是场强阈值太大覆盖范围为0或目标路径有误";
+  //   return false;
+  // }
+  return true;
+}
+
+void CFieldStrengthAnalyse::ComputeOneStation(Station& para, std::string progress = "") {
+  if (progress.empty()) {
+    progress = para.name;
+    progress = "计算" + GetModelName() + "场强-" + progress;
+  }
+  std::vector<double_t> rsv;
+  PrepareReservedValues(para, rsv);
+  ComputeOneStationByExtent(para, rsv);
+}
+
+bool CFieldStrengthAnalyse::ComputeOneStationByExtent(Station& para, std::vector<double_t>& rsv) {
+  //
+  int MaxRadius = std::max(cols, rows);
+  // 计算当前分析点的行列号
+  int Col, Row;
+  Col = (para.x - xMin) / cellSize;
+  if (Col < 0)
+    Col = 0;
+  else if (Col >= cols)
+    Col = cols - 1;
+  Row = (yMax - para.y) / cellSize;
+  if (Row < 0)
+    Row = 0;
+  else if (Row >= rows)
+    Row = rows - 1;
+  // 分析范围默认为DEM范围
+  int tempextent[4] = {0, cols, 0, rows};
+  int* pextent = tempextent;
+  int sr = 0;
+  int er = MaxRadius;
+  // 计算分析范围所属行列号范围
+  if (subExtent[0] != 0 || subExtent[1] != 0 || subExtent[2] != 0 || subExtent[3] != 0) {
+    pextent = GetSubRowCol(subExtent[0], subExtent[1], subExtent[2], subExtent[3]);
+    // 计算分析点到分析范围内移动的最小与最大行列数
+    int* movecr = new int[4];
+    movecr[0] = abs(Col - pextent[0]);
+    movecr[1] = abs(Col - pextent[1]);
+    movecr[2] = abs(Row - pextent[2]);
+    movecr[3] = abs(Row - pextent[3]);
+    // 计算移动的起止行列索引
+    sr = movecr[0];
+    er = movecr[0];
+    for (int i = 0; i < 4; i++) {
+      if (movecr[i] < sr) {
+        sr = movecr[i];
+      }
+      if (movecr[i] > er) {
+        er = movecr[i];
+      }
+    }
+    // 如果台站位于分析范围内则起止行列索引为0
+    if (para.x - subExtent[0] >= 0 && para.y - subExtent[1] >= 0 && para.x - subExtent[2] <= 0
+        && para.y - subExtent[3] <= 0) {
+      sr = 0;
+    }
+  }
+  CRect ComputeRect(pextent[0], pextent[2], pextent[1], pextent[3]);
+
+  if (needComputeAll) {
+    sr = 0;
+    er = MaxRadius;
+    ComputeRect.left = 0;
+    ComputeRect.right = cols;
+    ComputeRect.top = 0;
+    ComputeRect.bottom = rows;
+    pextent[0] = 0;
+    pextent[1] = cols;
+    pextent[2] = 0;
+    pextent[3] = rows;
+  }
+  // 从最小行列号开始分析
+  for (int Radius = sr; Radius < er; Radius++) {
+    bool HasCompute = false;
+    if ((Col - Radius < 0) && (Col + Radius >= cols) && (Row - Radius < 0)
+        && (Row + Radius >= rows)) {
+      break;
+    }
+    double_t X, Y;
+    float_t Z;
+    int i, j;
+    long Posi;
+    float_t result;
+
+    long nodata = noData;
+    // 上
+    i = Row - Radius;
+    if (i >= 0 && i >= pextent[2] && i <= pextent[3]) {
+      Y = yMax - cellSize * i - cellSize / 2;
+      int FromX = ComputeRect.left;
+      if (FromX < 0) FromX = 0;
+      int ToX = ComputeRect.right;
+      if (ToX >= cols) ToX = cols - 1;
+      X = xMin + FromX * cellSize + cellSize / 2;
+      Posi = FromX + i * cols;
+      for (j = FromX; j <= ToX; j++) {
+        pElevData->GetValueAsFloat(Posi, &Z);
+        if (nodata == (long)Z) {
+          X += cellSize;
+          Posi++;
+          continue;
+        }
+        OGRPoint p(X, Y, Z);
+        result = GetRadiuValue(para, rsv, p) + offsetDB;
+        if (nodata == (long)result) {
+          X += cellSize;
+          Posi++;
+          continue;
+        }
+        if (result >= para.freqThreshold) {
+          float FormerValue;
+          pData->GetValueAsFloat(Posi, &FormerValue);
+          if ((long)FormerValue == (long)noData) {
+            pData->SetValueAsFloat(Posi, result);
+          } else if (result > FormerValue) {
+            pData->SetValueAsFloat(Posi, result);
+          }
+          HasCompute = true;
+        }
+        X += cellSize;
+        Posi++;
+      }
+    }
+    // 下
+    i = Row + Radius;
+    if (i < rows && i <= pextent[3] && i >= pextent[2]) {
+      Y = yMax - cellSize * i - cellSize / 2;
+      int FromX = ComputeRect.left;
+      if (FromX < 0) FromX = 0;
+      int ToX = ComputeRect.right;
+      if (ToX >= cols) ToX = cols - 1;
+      X = xMin + FromX * cellSize + cellSize / 2;
+      Posi = FromX + i * cols;
+      for (j = FromX; j <= ToX; j++) {
+        pElevData->GetValueAsFloat(Posi, &Z);
+        if (nodata == (long)Z) {
+          X += cellSize;
+          Posi++;
+          continue;
+        }
+        OGRPoint point(X, Y, Z);
+        result = GetRadiuValue(para, rsv, point) + offsetDB;
+        if (nodata == (long)result) {
+          X += cellSize;
+          Posi++;
+          continue;
+        }
+        if (result >= para.freqThreshold) {
+          float FormerValue;
+          pData->GetValueAsFloat(Posi, &FormerValue);
+          if (nodata == (long)FormerValue) {
+            pData->SetValueAsFloat(Posi, result);
+          } else if (result > FormerValue) {
+            pData->SetValueAsFloat(Posi, result);
+          }
+          HasCompute = true;
+        }
+        X += cellSize;
+        Posi++;
+      }
+    }
+    // 左
+    j = Col - Radius;
+    if (j >= 0 && j >= pextent[0] && j <= pextent[1]) {
+      X = xMin + cellSize * j + cellSize / 2;
+      int FromY = ComputeRect.top + 1;
+      if (FromY < 0) FromY = 0;
+      int ToY = ComputeRect.bottom - 1;
+      if (ToY >= rows) ToY = rows - 1;
+      Y = yMax - FromY * cellSize - cellSize / 2;
+      Posi = FromY * cols + j;
+      for (i = FromY; i <= ToY; i++) {
+        pElevData->GetValueAsFloat(Posi, &Z);
+        if (nodata == (long)Z) {
+          Y -= cellSize;
+          Posi += cols;
+          continue;
+        }
+        OGRPoint point(X, Y, Z);
+        result = GetRadiuValue(para, rsv, point) + offsetDB;
+        if ((long)result == (long)noData) {
+          Y -= cellSize;
+          Posi += cols;
+          continue;
+        }
+        if (result >= para.freqThreshold) {
+          float FormerValue;
+          pData->GetValueAsFloat(Posi, &FormerValue);
+          if (nodata == (long)FormerValue) {
+            pData->SetValueAsFloat(Posi, result);
+          } else if (result > FormerValue) {
+            pData->SetValueAsFloat(Posi, result);
+          }
+          HasCompute = true;
+        }
+        Y -= cellSize;
+        Posi += cols;
+      }
+    }
+    // 右
+    j = Col + Radius;
+    if (j < cols && j <= pextent[1] && j >= pextent[0]) {
+      X = xMin + cellSize * j + cellSize / 2;
+      int FromY = ComputeRect.top + 1;
+      if (FromY < 0) FromY = 0;
+      int ToY = ComputeRect.bottom - 1;
+      if (ToY >= rows) ToY = rows - 1;
+      Y = yMax - FromY * cellSize - cellSize / 2;
+      Posi = FromY * cols + j;
+      for (i = FromY; i <= ToY; i++) {
+        pElevData->GetValueAsFloat(Posi, &Z);
+        if (nodata == (long)Z) {
+          Y -= cellSize;
+          Posi += cols;
+          continue;
+        }
+        OGRPoint point(X, Y, Z);
+        result = GetRadiuValue(para, rsv, point) + offsetDB;
+        if ((long)result == (long)noData) {
+          Y -= cellSize;
+          Posi += cols;
+          continue;
+        }
+        if (result >= para.freqThreshold) {
+          float FormerValue;
+          pData->GetValueAsFloat(Posi, &FormerValue);
+          if (nodata == (long)FormerValue) {
+            pData->SetValueAsFloat(Posi, result);
+          } else if (result > FormerValue) {
+            pData->SetValueAsFloat(Posi, result);
+          }
+          HasCompute = true;
+        }
+        Y -= cellSize;
+        Posi += cols;
+      }
+    }
+
+    if (!needComputeAll && !HasCompute) {
+      break;
+    }
+  }
+  return true;
+}
+
+int* CFieldStrengthAnalyse::GetSubRowCol(double xmin, double ymin, double xmax, double ymax) {
+  // 注意：在堆上分配了内存，注意释放
+  int* result = new int[4];
+  int colmin, rowmin, colmax, rowmax;
+  colmin = (xmin - xMin) / cellSize;
+  colmax = (xmax - xMin) / cellSize;
+  rowmin = (yMax - ymax) / cellSize;
+  rowmax = (yMax - ymin) / cellSize;
+
+  result[0] = colmin;
+  result[1] = colmax;
+  result[2] = rowmin;
+  result[3] = rowmax;
+  return result;
+}
+
+// bool CFieldStrengthAnalyse::SaveOutput(IFileFloatArray* pData, RasterCreateFileType type,
+//                                        RasterCreateDataType dtype) {
+//   int l = cols - 1, t = rows - 1, r = 0, b = 0;
+//   long pos = 0;
+//   long nodata = (long)noData;
+//   for (int i = 0; i < rows; i++) {
+//     for (int j = 0; j < cols; j++) {
+//       float fV;
+//       pData->GetValueAsFloat(pos, &fV);
+//       if (nodata != (long)fV) {
+//         if (l > j) l = j;
+//         if (r < j) r = j;
+//         if (t > i) t = i;
+//         if (b < i) b = i;
+//       }
+//       pos++;
+//     }
+//   }
+//   if ((l > r) || (t > b)) return false;
+//   IPoint* LeftTop;
+//   ::CoCreateInstance(CLSID_PointDef, NULL, CLSCTX_INPROC_SERVER, IID_IPoint, (LPVOID*)&LeftTop);
+//   LeftTop->put_X(XMin + l * CellSize);
+//   LeftTop->put_Y(YMax - t * CellSize);
+//   int rCols;
+//   int rRows;
+//   double_t OutputCellSize;
+//   pEnvi->get_OutputCellSize(&OutputCellSize);
+//   rCols = (r * CellSize - l * CellSize + CellSize) / OutputCellSize;
+//   rRows = (b * CellSize - t * CellSize + CellSize) / OutputCellSize;
+//   IGDALRasterCreator* pCreator;
+//   ISpatialReference* pSpatial;
+//   IGDALRasterProperties* pPro;
+//   pElevs->QueryInterface(IID_IGDALRasterProperties, (void**)&pPro);
+//   pPro->Release();
+//   pPro->get_SpatialReference(&pSpatial);
+//   ::CoCreateInstance(CLSID_GDALRasterCreator, NULL, CLSCTX_INPROC_SERVER, IID_IGDALRasterCreator,
+//                      (LPVOID*)&pCreator);
+//   if (progress != NULL) progress->BeginProgress(CComBSTR("创建结果图层"));
+//   VARIANT_BOOL IsOk;
+//   pCreator->CreateRaster(CComBSTR(OutPath), LeftTop, rCols, rRows, OutputCellSize, type, dtype,
+//   1,
+//                          pSpatial, NoData, &IsOk);
+//   pCreator->Release();
+//   pSpatial->Release();
+//   LeftTop->Release();
+//   if (!IsOk) {
+//     ErrorInfo = "创建数据失败";
+//     return false;
+//   }
+//   IGDALRasterSaverByFileArray* pSaver;
+//   ::CoCreateInstance(CLSID_GDALRasterSaverByFileArray, NULL, CLSCTX_INPROC_SERVER,
+//                      IID_IGDALRasterSaverByFileArray, (LPVOID*)&pSaver);
+//   pSaver->OpenRaster(CComBSTR(OutPath), &IsOk);
+//   if (!IsOk) {
+//     ErrorInfo = "保存数据失败";
+//     pSaver->Release();
+//     return false;
+//   }
+//   VARIANT pBand;
+//   pBand.vt = VT_I4;
+//   pBand.intVal = 1;
+//   pSaver->PutRasterBand(pBand, &IsOk);
+//   if (!IsOk) {
+//     ErrorInfo = "保存数据失败";
+//     pSaver->Release();
+//     return false;
+//   }
+//   if ((dtype == rcdtFloat32) || (dtype == rcdtFloat64))
+//     pSaver->SaveBlockDataWithInterpolateByRect(0, 0, rCols - 1, rRows - 1, l, t, r, b, Cols,
+//     Rows,
+//                                                pData, progress, &IsOk);
+//   else
+//     pSaver->SaveBlockDataByRect(0, 0, rCols - 1, rRows - 1, l, t, r, b, Cols, Rows, pData,
+//     progress,
+//                                 &IsOk);
+//   if (!IsOk) {
+//     ErrorInfo = "保存数据失败";
+//     pSaver->Release();
+//     return false;
+//   }
+//   pSaver->Release();
+//   return true;
+// }
+
+/*** ----------------------CCombineAnalyse 混合传播模型的实现------------------------------ */
+
+float_t CCombineAnalyse::GetRadiuValue(Station& stationInfo, std::vector<double_t>& rsv,
+                                       OGRPoint* point) {
+  // 可能有多个绕射模型，所以els中可能有多个不同的绕射模型。
+  double_t X = point->getX();
+  double_t Y = point->getY();
+  double_t Z = point->getZ();
+  double_t allNum = 0;
+  for (int k = els.size() - 1; k >= 0; k--) {
+    double_t loss;
+    els.at(k)->GetDBLoss(stationInfo, X, Y, Z, &loss);
+    allNum += loss;
+  }
+  return allNum;
+}
+
+bool CCombineAnalyse::FieldStrengthAnalyse(std::string savePath, RasterCreateFileType type) {
+  bool isOk = CFieldStrengthAnalyse::FieldStrengthAnalyse(savePath, type);
+  return isOk;
+}
+
+/*** ----------------------CFreeSpaceAnalyse 自由空间传播模型的实现------------------------------ */
+
+/// @brief 自由传播算法的实现
+/// @param stationInfo 站信息
+/// @param rsv
+/// @param point 要计算的点坐标
+/// @return 返回point位置处的场强
+float_t CFreeSpaceAnalyse::GetRadiuValue(Station& stationInfo, std::vector<double_t>& rsv,
+                                         OGRPoint* point) {
+  float_t tfV;
+  double_t X = point->getX();
+  double_t Y = point->getY();
+  double_t Z = point->getZ();
+  float d = sqrt(pow(stationInfo.x - X, (double)2.0) + pow(stationInfo.y - Y, (double)2.0)
+                 + pow(stationInfo.stationHeight + stationInfo.dem - Z - hm, (double)2.0));
+  tfV = 32.4 + 20 * log10(d / 1000) + 20 * log10(stationInfo.frequency);
+  OGRPoint point(X, Y, Z);
+  double_t dbLoss = CCombineAnalyse::GetRadiuValue(stationInfo, rsv, point);
+  // return rsv.ReservedValues[0] - tfV - dbLoss;
+  return rsv.front() - tfV - dbLoss;
 }
