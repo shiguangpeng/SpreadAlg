896ba09ef9262b1789f9d69aa69c88700dd27049
shiguangpeng <742508176@qq.com>
2024-03-13 14:37:19 +0800

    :sparkles: 测试完成，待后续优化。

--- test/source/test.cpp
+++ test/source/test.cpp
@@ -2,12 +2,15 @@
 
 #include <doctest/doctest.h>
 // #include <spread/spatidatamanager.h>
+#include <math.h>
 #include <spread/spread.h>
 
+#include <fstream>
+#include <iostream>
 #include <string>
 
-/// @brief 空间数据管理的测试用例
-TEST_SUITE("空间数据管理的测试用例") {
+/// @brief 传播算法测试SUITE
+TEST_SUITE("传播算法测试用例") {
   // TEST_CASE("SPAT") {
   //   using namespace spatidatamanager;
   //   GDALDataset* result = RasterDataSource::OpenRaster("F:\\work\\spreadmodel\\test.tif");
@@ -15,7 +18,7 @@ TEST_SUITE("空间数据管理的测试用例") {
 
   TEST_CASE("自由传播模型调用测试") {
     using namespace spread;
-    std::string path = "/home/shigp/data/test.tif";
+    std::string path = "/home/shigp/data/test_prj.tif";
     // CSpreadAnalyse sp;
     // sp.elevationPath = path;
     // sp.InitEnvironment();
@@ -23,9 +26,13 @@ TEST_SUITE("空间数据管理的测试用例") {
     // 测试坐标
     // 103.556378  31.566910
     Station station;
-    station.lontitude = 103.556378;
-    station.latitude = 31.566910;
+    station.x = 11502076.828;
+    station.y = 3505543.756;
     station.frequency = 107.2;
+    station.stationHeight = 30;
+    station.power = 10 * log10(100);
+    station.freqThreshold = 40;
+    station.dem = 499;
     // CStations stations;
     // stations.AddStation(&station);
     analyse->elevationPath = path;
@@ -36,11 +43,23 @@ TEST_SUITE("空间数据管理的测试用例") {
         "/home/shigp/data", RasterCreateFileType::rcftTiff, &flag);
     std::cout << "return flag is:" << flag << std::endl;
     IFileFloatArray *arr = analyse->pData;
-    long cout = 0;
-    arr->GetSize(&cout);
-    for (long i = 0; i < cout; i++) {
-      float *p = arr->GetRasterDataArray();
-      std::cout << p[i] << std::endl;
+    long count = 0;
+    arr->GetSize(&count);
+
+    float *data = arr->GetRasterDataArray();
+    // 打开文件进行写操作
+    std::ofstream outfile;
+    outfile.open("/home/shigp/data/result.csv", std::ios::out);
+
+    // 写入数组内容到文件
+    for (long i = 0; i < count; i++) {
+      if (i == count - 1) {
+        outfile << data[i] << std::endl;
+      } else {
+        outfile << data[i] << ",";
+      }
     }
+    // 关闭文件
+    outfile.close();
   }
 }
